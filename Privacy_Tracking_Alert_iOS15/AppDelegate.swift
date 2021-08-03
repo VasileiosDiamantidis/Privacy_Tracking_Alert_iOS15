a154c686a30cf3f070201458fae129be427a9a3b
VasileiosDiamantidis <vasileios.diamantidis@zattoo.com>
2021-08-03 16:01:50 +0200

    Remove delay as it does not affect the issue

--- Privacy_Tracking_Alert_iOS15/AppDelegate.swift
+++ Privacy_Tracking_Alert_iOS15/AppDelegate.swift
@@ -16,11 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setUpWindow()
+        
         requestCameraPermission()
         requestNotificationPermission()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25) {
-            self.requestTrackingAuthorization()
-        }
+        requestTrackingAuthorization()
+        
         return true
     }
     
